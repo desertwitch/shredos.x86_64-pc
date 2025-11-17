b4f8a1f6a84f4dfca0a03f7eb6a9babdc3a271b8
desertwitch <24509509+desertwitch@users.noreply.github.com>
2025-11-17 11:59:53 +0100

    fix: consider different architectures

--- board/shredos/make_img_file.sh
+++ board/shredos/make_img_file.sh
@@ -1,29 +1,37 @@
 #!/bin/bash -e
 
+if grep -Eq "^BR2_i386=y$" "${BR2_CONFIG}"; then
+	MKIMAGE_ARCH=i386
+	MKIMAGE_EFI=bootia32.efi
+else
+	MKIMAGE_ARCH=x86_64
+	MKIMAGE_EFI=bootx64.efi
+fi
+
 version=$(cat board/shredos/fsoverlay/etc/shredos/version.txt)
 
-cp "board/shredos/grub.cfg"                         "${BINARIES_DIR}/grub.cfg"      || exit 1
-cp "output/target/lib/grub/i386-pc/boot.img"        "${BINARIES_DIR}/boot.img"      || exit 1
-cp "{$BINARIES_DIR}/efi-part/EFI/BOOT/bootx64.efi"  "${BINARIES_DIR}/bootx64.efi"   || exit 1
+cp "board/shredos/grub.cfg"                         "${BINARIES_DIR}/grub.cfg"            || exit 1
+cp "output/target/lib/grub/i386-pc/boot.img"        "${BINARIES_DIR}/boot.img"            || exit 1
+cp "{$BINARIES_DIR}/efi-part/EFI/BOOT/${MKIMAGE_EFI}"  "${BINARIES_DIR}/${MKIMAGE_EFI}"   || exit 1
 
-cp "board/shredos/autorun.inf"             "${BINARIES_DIR}/autorun.inf"            || exit 1
-cp "board/shredos/README.txt"              "${BINARIES_DIR}/README.txt"             || exit 1
-cp "board/shredos/shredos.ico"             "${BINARIES_DIR}/shredos.ico"            || exit 1
+cp "board/shredos/autorun.inf"             "${BINARIES_DIR}/autorun.inf"                  || exit 1
+cp "board/shredos/README.txt"              "${BINARIES_DIR}/README.txt"                   || exit 1
+cp "board/shredos/shredos.ico"             "${BINARIES_DIR}/shredos.ico"                  || exit 1
 
 # version.txt is used to help identify the (boot) USB disk
-cp "board/shredos/fsoverlay/etc/shredos/version.txt" "${BINARIES_DIR}/version.txt"  || exit 1
+cp "board/shredos/fsoverlay/etc/shredos/version.txt" "${BINARIES_DIR}/version.txt"        || exit 1
 
-rm -rf "${BUILD_DIR}/genimage.tmp"                                                  || exit 1
+rm -rf "${BUILD_DIR}/genimage.tmp"                                                        || exit 1
 genimage --rootpath="${TARGET_DIR}" \
     --inputpath="${BINARIES_DIR}" \
     --outputpath="${BINARIES_DIR}" \
-    --config="board/shredos/genimage.cfg" \
-    --tmppath="${BUILD_DIR}/genimage.tmp"                                           || exit 1
+    --config="board/shredos/genimage_${MKIMAGE_ARCH}.cfg" \
+    --tmppath="${BUILD_DIR}/genimage.tmp"                                                 || exit 1
 
 SUFFIXIMG="${version}_$(date +%Y%m%d)"
 FINAL_IMAGE_PATH="${BINARIES_DIR}/shredos-${SUFFIXIMG}.img"
-mv "${BINARIES_DIR}/shredos.img" "${FINAL_IMAGE_PATH}"                              || exit 1
+mv "${BINARIES_DIR}/shredos.img" "${FINAL_IMAGE_PATH}"                                    || exit 1
 
-echo "USB image '${FINAL_IMAGE_PATH}' created successfully!"
+echo "USB image '${FINAL_IMAGE_PATH}' (for '${MKIMAGE_ARCH}' architecture) created successfully!"
 
 exit 0
--- board/shredos/make_iso_file.sh
+++ board/shredos/make_iso_file.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+if grep -Eq "^BR2_i386=y$" "${BR2_CONFIG}"; then
+	MKIMAGE_ARCH=i386
+else
+	MKIMAGE_ARCH=x86_64
+fi
+
 version=$(cat board/shredos/fsoverlay/etc/shredos/version.txt)
 
 SUFFIXISO="${version}_$(date +%Y%m%d)"
@@ -7,4 +13,4 @@ FINAL_ISO_PATH="${BINARIES_DIR}/shredos-${SUFFIXISO}.iso"
 
 mv "${BINARIES_DIR}/rootfs.iso9660" "${FINAL_ISO_PATH}"
 
-echo "ISO image '${FINAL_ISO_PATH}' created successfully!"
+echo "ISO image '${FINAL_ISO_PATH}' (for '${MKIMAGE_ARCH}' architecture) created successfully!"
