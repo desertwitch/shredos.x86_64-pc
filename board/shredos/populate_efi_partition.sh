77dfd8cb0e71ae85d62bd7f86566c8b52a268db6
desertwitch <24509509+desertwitch@users.noreply.github.com>
2025-11-18 18:18:08 +0100

    chore: wording

--- board/shredos/populate_efi_partition.sh
+++ board/shredos/populate_efi_partition.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 #
+# BR2_ROOTFS_POST_BUILD_SCRIPT for shredos_iso_aio configurations
+#
 # Our hybrid all-in-one ISO has an accessible EFI partition, which we
 # can (ab)use to store our information also, so we move the usual data
 # onto it before creating the images. This should not cause any issues
 # for CD/DVD-ROM burned ISO images, as the EFI partition will be in RAM,
-# but allow any USB burned ISO images to have that accessible data location.
+# but allow any USB burned ISO images to have that writeable data location.
 #
 # Keep in mind that the EFI partition size (in configuration) must be below
-# the maximum of 65535 blocks of 512 bytes (= ~32 MB) or the ISO will not
-# be bootable, but xorriso would warn about this in the ISO creation stage.
+# the maximum of 65535 blocks of 512 bytes (= ~32 MB) or the ISO may not
+# be bootable on legacy systems, xorriso would warn about this during the
+# ISO building stage (but not consider it a failure - just so you know...)
 #
 
 mkdir -p "${BINARIES_DIR}/efi-part/boot/"                                                               || exit 1
